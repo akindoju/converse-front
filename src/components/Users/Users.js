910ffd5ec2204a3b2afafcb21a10b7ddc478a6b4
Akndju <davidd.akindoju@gmail.com>
2021-06-10 11:31:49 +0100

    Created client side project -converse

---
+++ src/components/Users/Users.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import './Users.css';
+
+const Users = ({ activeUsers }) => {
+  useEffect(() => {
+    console.log(activeUsers);
+  });
+
+  return (
+    <div className="usersContainer">
+      <header className="activeUsersHeader">Active Users</header>
+      {activeUsers && (
+        <ul>
+          {activeUsers.map((user) => {
+            return <li key={user.id}>{user.name}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Users;
--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -6,17 +6,19 @@ import InfoBar from '../../components/InfoBar/InfoBar';
 import Input from '../../components/Input/Input';
 import Messages from '../../components/Messages/Messages';
 import Header from '../../components/Header/Header';
+import Users from '../../components/Users/Users';
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [activeUsers, setActiveUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
-  // const ENDPOINT = 'localhost:5000/';
-  const ENDPOINT = 'https://akindoju-converse.herokuapp.com/';
+  const ENDPOINT = 'localhost:5000/';
+  // const ENDPOINT = 'https://akindoju-converse.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search); //to get data passed in as URL from Join component
@@ -34,6 +36,12 @@ const Chat = ({ location }) => {
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
+    socket.on('roomData', ({ users }) => {
+      setActiveUsers(users);
+    });
+  }, []);
+
+  useEffect(() => {
     socket.on('message', (message) => {
       setMessages((messages) => [...messages, message]); //spreading in current messages and adding a new one to it
     });
@@ -43,25 +51,26 @@ const Chat = ({ location }) => {
     event.preventDefault();
 
     if (message) {
-      //setMessage is the calback fn from the serverside
+      //setMessage is the callback fn from the serverside
       socket.emit('sendMessage', message, () => setMessage(''));
     }
-
-    console.log(message, messages);
   };
 
   return (
     <div>
       <Header />
       <div className="outerContainer">
-        <div className="container">
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input
-            message={message}
-            setMessage={setMessage}
-            sendMessage={sendMessage}
-          />
+        <div className="chatContainer">
+          <div className="container">
+            <InfoBar room={room} />
+            <Messages messages={messages} name={name} />
+            <Input
+              message={message}
+              setMessage={setMessage}
+              sendMessage={sendMessage}
+            />
+          </div>
+          <Users activeUsers={activeUsers} />
         </div>
       </div>
     </div>
